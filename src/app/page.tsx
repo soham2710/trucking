8abfef339ece3922758d753a08f7e2828e79d02c
Soham Sharma <sohamnsharma@gmail.com>
2024-12-25 07:08:13 +0530

    page change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Hero from './components/Hero';
 import AboutUs from './components/AboutUs';
 import Services from './components/Services';
 import Carousel from './components/Carousel';
-import Newsletter from './components/Newsletter';
 
 import Footer from './components/Footer';
 import Navigation from './components/Navigation';
