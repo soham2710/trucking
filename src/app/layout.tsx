0daf7e0f04e35cf00a4968f998e07fa3f69ee7ab
Soham Sharma <sohamnsharma@gmail.com>
2025-01-03 08:13:04 +0530

    googleanalytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 // app/layout.tsx
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Head from 'next/head'; // Import the Head component
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -18,6 +19,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth">
+      <Head>
+        {/* Google Analytics Tag */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-WHZZFH2P2E"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-WHZZFH2P2E');
+            `,
+          }}
+        />
+      </Head>
       <body className={inter.className}>
         <main>{children}</main>
       </body>
