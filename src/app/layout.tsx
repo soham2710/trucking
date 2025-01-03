f9235732804f7a659f041a9371dc2c1a25c3d105
Soham Sharma <sohamnsharma@gmail.com>
2025-01-03 08:37:53 +0530

    mixpanel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 // app/layout.tsx
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import Head from 'next/head'; // Import the Head component
 import './globals.css';
+import mixpanel from 'mixpanel-browser'; // Import Mixpanel
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,6 +12,13 @@ export const metadata: Metadata = {
   keywords: 'trucking services, logistics company, freight shipping, LTL shipping, FTL transport, nationwide logistics',
 };
 
+// Initialize Mixpanel
+mixpanel.init("aaa18717adfa752e6898ec2bf2fd06c5", {
+  debug: true,
+  track_pageview: true,
+  persistence: "localStorage",
+});
+
 export default function RootLayout({
   children,
 }: {
@@ -19,20 +26,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth">
-      <Head>
-        {/* Google Analytics Tag */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-WHZZFH2P2E"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-WHZZFH2P2E');
-            `,
-          }}
-        />
-      </Head>
       <body className={inter.className}>
         <main>{children}</main>
       </body>
