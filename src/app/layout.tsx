61655b716bceb0249c85401a65296f648c3817ef
Soham Sharma <sohamnsharma@gmail.com>
2025-01-29 16:35:04 +0530

    tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,23 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-56R5VF28');
+            `,
+          }}
+        />
+
         {/* Google Analytics */}
         {GA_MEASUREMENT_ID && (
           <>
@@ -31,8 +48,6 @@ export default function RootLayout({
                   window.dataLayer = window.dataLayer || [];
                   function gtag(){dataLayer.push(arguments);}
                   gtag('js', new Date());
-
-                  // Google Analytics
                   gtag('config', '${GA_MEASUREMENT_ID}');
                 `,
               }}
@@ -55,8 +70,6 @@ export default function RootLayout({
                   window.dataLayer = window.dataLayer || [];
                   function gtag(){dataLayer.push(arguments);}
                   gtag('js', new Date());
-
-                  // Google Ads
                   gtag('config', '${GOOGLE_ADS_ID}');
                 `,
               }}
@@ -77,7 +90,6 @@ export default function RootLayout({
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-
               gtag('config', 'AW-16818747005');
             `,
           }}
@@ -85,7 +97,16 @@ export default function RootLayout({
       </head>
       <body className={inter.className}>
         <main>{children}</main>
-        
+
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-56R5VF28"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+
         {/* Conversion Tracking Script */}
         <Script
           id="conversion-tracking"
